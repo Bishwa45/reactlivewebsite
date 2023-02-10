186daa1dd570523ea6cc9153ce7fcd5f19348cd9
Bishwa45 <pokhrelbishwa28@gmail.com>
2023-02-10 16:56:21 +0545

    Updates

---
+++ precache-manifest.d2aeeb9e56fffdb50268cf40f118bd54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1464f3aef87e33187831",
+    "url": "/%20reactlivewebsite/static/css/main.f04b8fe7.chunk.css"
+  },
+  {
+    "revision": "1464f3aef87e33187831",
+    "url": "/%20reactlivewebsite/static/js/main.1464f3ae.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/%20reactlivewebsite/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/%20reactlivewebsite/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "252595a2bef4458d501f",
+    "url": "/%20reactlivewebsite/static/js/runtime~main.252595a2.js"
+  },
+  {
+    "revision": "3a3ceba2234ec1ae3bb844ec77ceef4a",
+    "url": "/%20reactlivewebsite/index.html"
+  }
+];
